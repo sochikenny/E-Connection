5e16315b4a2b24c3aad5ae4caf1674929ad5088a
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2021-04-28 07:31:27 -0400

    user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { login } from '../../actions/auth'
 
-const Login = () => {
+const Login = ({ login }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -13,7 +16,7 @@ const Login = () => {
 
     const onSubmit = async e => {
     e.preventDefault()
-    console.log('Success')
+    login(email, password)
     }
 
     return (
@@ -48,5 +51,10 @@ const Login = () => {
     )
 }
 
-export default Login
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+}
+
+
+export default connect(null, {login})(Login)
 
